356023db3191b07f4829f03ebecfaa0e46f43dd4
ManuCoolN <n.manohar2006@gmail.com>
2025-09-07 21:19:01 -0400

    fourth version

--- movies/views.py
+++ movies/views.py
@@ -8,10 +8,9 @@ def index(request):
         movies = Movie.objects.filter(name__icontains=search_term)
     else:
         movies = Movie.objects.all()
-
     template_data = {}
     template_data['title'] = 'Movies'
-    template_data['movies'] = movies
+    template_data['movies'] = Movie.objects.all()
     return render(request, 'movies/index.html', {'template_data': template_data})
 
 def show(request, id):
